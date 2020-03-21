2c68e559ac46b0cc99ab3ea50f9c6dc2e2199c7c
zhongjiahao <zhongjiahao@yy.com>
2020-03-21 15:36:42 +0800

    feat: 修正命名问题

--- @wxparcel-transformer/base/src/typings.ts
+++ @wxparcel-transformer/base/src/typings.ts
@@ -1,15 +1,15 @@
-export interface ParseParams {
+export interface TransformerParams {
   file: string
   source: Buffer
 }
 
-export interface ParseResult {
+export interface TransformerResult {
   code: Buffer
   map: Object
   ast: Object
   dependencies: string[]
 }
 
-export interface ParseAlias {
+export interface Alias {
   [key: string]: string[]
 }
